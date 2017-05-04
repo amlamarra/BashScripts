8278ca872b335dd215b5d31f8f950f069f3d4bfd
Andrew Lamarra <andrew.lamarra@gmail.com>
2017-05-04 14:46:02 -0400

    Update ovpn_iptables.sh

--- ovpn_iptables.sh
+++ ovpn_iptables.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Create the necessary iptables entries for OpenVPN
+# Create the necessary iptables entries for an OpenVPN server
 
 iptables -I FORWARD -i tun0 -o eth0 -s 10.8.0.0/24 -m conntrack --ctstate NEW -j ACCEPT
 iptables -I FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
