c5f3e699ab1701de0876d8cdef73df12b57009c1
Andrew Lamarra <andrew.lamarra@gmail.com>
2017-05-04 14:53:44 -0400

    Update test_cipher_suites.sh

--- test_cipher_suites.sh
+++ test_cipher_suites.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/env bash
 # Display all available cipher suites from a hosted web server
 
-read -p "Enter the IP or domain name: " domain
+read -rp "Enter the IP or domain name: " domain
 # OpenSSL requires the port number.
 SERVER="$domain":443
 
 ciphers=$(openssl ciphers 'ALL:eNULL' | sed -e 's/:/ /g')
 
-echo Obtaining cipher list from $(openssl version).
+echo "Obtaining cipher list from $(openssl version)."
 
-for cipher in ${ciphers[@]}
-do
+for cipher in "${ciphers[@]}"; do
+    result=$(echo -n | openssl s_client -cipher "$cipher" -connect "$SERVER" 2>&1)
 
-result=$(echo -n | openssl s_client -cipher "$cipher" -connect $SERVER 2>&1)
+    if [[ "$result" =~ "Cipher is ${cipher}" || "$result" =~ "Cipher    :" ]]; then
+        echo "$cipher"
+    fi
 
-if [[ "$result" =~ "Cipher is ${cipher}" || "$result" =~ "Cipher    :" ]] ; then
-    echo $cipher
-fi
-
-sleep 1
+    sleep 1
 done
