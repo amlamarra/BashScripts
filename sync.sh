5fc64bf783a8282a286d896a9675dd3754f92f8f
Andrew Lamarra <andrew.lamarra@gmail.com>
2019-01-23 10:39:07 -0500

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -11,29 +11,56 @@
 to_sync=~/tools/list.txt
 base=~/tools
 
+urls=""
+# Get the list of Git repos in list.txt
 readarray -t urls < $to_sync
+
+# Get the list of directories in ~/tools/
+dirs=( $(find $base -maxdepth 1 -mindepth 1 -type d) )
+
+function repo_not_found {
+	#echo -e "\nRepository not found. Let's get it...\n"
+	echo -e "A repo in your list.txt file was not found."
+	read -rp "Would you like to clone it? (Y/n) " ans
+	ans=$(echo "$ans" | tr '[:upper:]' '[:lower:]')
+	if [[ $ans == '' ]]; then ans="Y"; fi
+}
 
 for url in "${urls[@]}"; do
 	counter=0
 	echo "Now checking to make sure $url is here."
-	dirs=( $(find $base -maxdepth 1 -mindepth 1 -type d) )
+	
+	# Print the current state of the $dirs variable
+	echo -e "\n\tCurrent dirs array:"
+	for i in "${dirs[@]}"; do echo -e "\t\t$i"; done
+	echo
+
 	while read dir; do
+		# Save the current number of elements in the dirs array
+		num_dirs="${#dirs[@]}"
+
 		echo -e "\tIs this it?  $dir"
 		cd "$dir"
 		if [[ $url == $(git config --get remote.origin.url) ]]; then
 			echo -e "\tFOUND IT!  Updating..."
-			git pull
-			break
+			#git pull
+			# Remove the found directory
+			unset "dirs[$counter]"
+			break 1
 		else
 			echo -e "\tNo. Continuing..."
 			counter=$((counter + 1))
 		fi
 	done < <(printf '%s\n' "${dirs[@]}")
-	num_dirs="${#dirs[@]}"
+
+	echo -e "\tCounter = $counter\n"
+
 	if [[ $counter -ge $num_dirs ]]; then
-		echo -e "\nRepository not found. Let's get it...\n"
-		cd "$base"
-		git clone "$url"
+		repo_not_found "$url"
 	fi
+
+	# Print the current state of the $dirs variable
+	echo -e "\tCurrent dirs array:"
+	for i in "${dirs[@]}"; do echo -e "\t\t$i"; done
 	echo
 done
