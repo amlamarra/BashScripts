389b4202fc34a7a5c5f2876966697b1ed13d461d
Andrew Lamarra <andrew.lamarra@gmail.com>
2019-01-23 12:42:51 -0500

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -23,7 +23,8 @@ dirs=( $(find $base -maxdepth 1 -mindepth 1 -type d) )
 
 function dir_not_found {
 	url="$1"
-	echo -e "A repo in your $list file was not found in $base."
+	echo "The following repository was found in your list but not in the $base/ directory:"
+	echo -e "\t$url"
 
 	# Ask the user about cloning the repo (and repeat if input is invalid)
 	ans=''
@@ -60,8 +61,10 @@ function repo_not_found {
 	url="$1"
 	dir="$2"
 
-	echo "The following repository was found in your $base/ directory:"
+	echo "The following repository was found in $dir/ but not your list:"
 	echo -e "\t$url"
+
+	# Ask the user about adding it to the list
 	ans=''
 	while [[ $ans != 'y' ]] && [[ $ans != 'n' ]]; do
 		read -rp "Would you like to add it to your $list file? (Y/n) " ans
@@ -82,7 +85,7 @@ function repo_not_found {
 		ans=$(echo "$ans" | tr '[:upper:]' '[:lower:]')
 	done
 
-	# If 'yes' then delete the line from the list of repos
+	# If 'yes' then delete the directory
 	if [[ $ans == 'y' ]]; then
 		rm -rf "$dir"
 	fi
@@ -91,43 +94,26 @@ function repo_not_found {
 # Iterate over the repos found in the list
 for url in "${urls[@]}"; do
 	counter=0
-	echo "Now checking to make sure $url is here."
 	
-	# Print the current state of the $dirs variable
-	echo -e "\n\tCurrent dirs array:"
-	for i in "${dirs[@]}"; do echo -e "\t\t$i"; done
-	echo
-
-	#while read dir; do
 	for dir in "${dirs[@]}"; do
 		# Save the current number of elements in the dirs array
 		num_dirs="${#dirs[@]}"
 
-		echo -e "\tIs this it?  $dir"
 		cd "$dir"
 		if [[ $url == $(git config --get remote.origin.url) ]]; then
-			echo -e "\tFOUND IT!  Updating..."
 			#git pull
 			# Remove the found directory
 			unset "dirs[$counter]"
 			break 1
 		else
-			echo -e "\tNo. Continuing..."
 			counter=$((counter + 1))
 		fi
-	#done < <(printf '%s\n' "${dirs[@]}")
 	done
-
-	echo -e "\tCounter = $counter\n"
 
 	if [[ $counter -ge $num_dirs ]]; then
 		dir_not_found "$url"
+		echo
 	fi
-
-	# Print the current state of the $dirs variable
-	echo -e "\tCurrent dirs array:"
-	for i in "${dirs[@]}"; do echo -e "\t\t$i"; done
-	echo
 done
 
 # Iterate through leftover dirs to see if any are repos not in the list
@@ -136,5 +122,6 @@ for dir in "${dirs[@]}"; do
 	url=$(git config --get remote.origin.url)
 	if [[ $? == 0 ]]; then
 		repo_not_found "$url" "$dir"
+		echo
 	fi
 done
